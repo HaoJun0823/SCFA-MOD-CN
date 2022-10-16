2acd7cbba5740d080485a9b4e1f1ffa34682ec99
HaoJun0823 <developer@haojun0823.xyz>
2022-10-16 15:16:00 +0800

    BrewLAN: Additional unit mods US語言加入

--- loc/CN/strings_db.lua
+++ loc/CN/strings_db.lua
@@ -7485,6 +7485,8 @@ _Campaign="模組戰役"
 tooltipui0135="模組戰役"
 tooltipui0136="用你選擇的模組開始或者繼續一個戰役。"
 
+# MOD::Jon Mod (1D6E5CAA-7D9B-11DF-9FAF-7D04DFD72085) Enables the Brackman Megalith to spawn in mission 5
+
 # BlackOps 
 # MOD::BlackOps Unleashed (9e8ea941-c306-4751-b367-a11000000502) Version 5.2 (Is Version 6 alive?)
 
@@ -8435,3 +8437,316 @@ Unit_Description_BrewLAN_T4L004="實驗性狙擊機器人，[希沙亞]雖然巨
 ssl0405_desc="實驗性節肢機器人"
 ssl0405_name="蘇達茲馬拉"
 Unit_Description_BrewLAN_T4L005="實驗性節肢機器人，[蘇達茲馬拉]配備了[索阿烏]和[辛烏特]加農炮,腹側有[烏奧]魚雷武器,外殼上有[歐拉斯]防空炮。[蘇達茲馬拉]可以垂直伸展增加武器射程，也可以游泳和潛艇作戰，可以摧毀所有目標單位。"
+
+# MOD::BrewLAN: Additional unit mods 0.899 (190261d0-4bb0-11e2-bcSC-BLNEWUNITS02) (套娃結構，裡面的就不記UUID了)
+
+# BrewTurrets
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Caffe Corretto EN-US strings
+srb5380_name="Deadshot"
+srb5380_desc="Accuracy Boost Node"
+Unit_Description_BrewLAN_Turrets_0001="Adjacent static weaponry spend longer aiming, resulting in higher accuracy. Multiple nodes stack."
+
+seb5381_name="Superhot"
+seb5381_desc="Damage Boost Node"
+Unit_Description_BrewLAN_Turrets_0002="Adjacent static weaponry deal increased damage. This greatly increases energy consumption per shot."
+
+seb5382_name="Benson"
+seb5382_desc="Factory Health Node"
+Unit_Description_BrewLAN_Turrets_0010="Adjacency buff node. Units built by adjacent factory structures will have additional health."
+
+srb5383_name="The Lost"
+srb5383_desc="Factory Stealth Toggle Node"
+Unit_Description_BrewLAN_Turrets_0011="Adjacency buff node. Units built by adjacent factory structures will have any stealth abilities set active or inactive instead of their default state."
+
+tooltipbrewturrets0002="Factory build stealth toggle"
+tooltipbrewturrets0003="Sets units build by adjacent factories to have stealth enabled."
+tooltipbrewturrets0004="Factory build cloak toggle"
+tooltipbrewturrets0005="Sets units build by adjacent factories to have cloak enabled."
+
+seb2320_name="Hiro"
+seb2320_desc="Heavy Point Defense"
+Unit_Description_BrewLAN_Turrets_0003="Heavy defensive tower. Armed with a plasma beam."
+
+seb2211_name="Stouty"
+sbui2311_desc="Anti-Armor Point Defense"
+Unit_Description_BrewLAN_Turrets_0004="Defensive tower. Armed with a destroyer grade plasma cannon."
+
+seb2111_name="Shorty"
+Unit_Description_BrewLAN_Turrets_0005="Light defensive tower. Armed with a frigate-grade plasma cannon."
+
+srb2402_name="Neolith"
+seb2401_desc="Experimental Point Defense"
+Unit_Description_BrewLAN_Turrets_0006="Defensive turret. Armed with a massive proton cannon."
+
+srb2391_name="Tesla Coil"
+srb2391_desc="Heavy Point Defense"
+Unit_Description_BrewLAN_Turrets_0007="Powerful, short range, defensive turret. It gradually stores energy and discharges everything with each shot. Most effective against large targets when given time to charge. Highly ineffective at sustained fire."
+
+--This is used by the Tesla Coil
+tooltipbrewturrets0000="Toggle weapon power"
+tooltipbrewturrets0001="Enables or disables main weapon power."
+
+seb2402_name="Maelstrom"
+seb2402_desc="Experimental Anti-Air Defense"
+Unit_Description_BrewLAN_Turrets_0008="Defensive anti-air weapon. Provides devastating short-range damage. Consumes power with each shot."
+
+srb2403_name="Tartarus"
+srb2403_desc="Experimental Point Defense"
+--"Defensive turret. Armed with a high intensity microwave laser."
+
+# BrewTeaParty.
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Tea Party strings
+sas0401_name="Deluge Class"
+sas0401_desc="Experimental Battlecruiser"
+Unit_Description_BrewTeaParty_001="Experimental dreadnought-type battlecruiser. Features light armor for increased movement speed, with personal shields and anti-torpedo weapons for defenses. Offensively armed with Oblivion cannons, shield disruption cannons, anti-air missiles, and a secondary fragmentation artillery."
+
+# BrewShields
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Bubble Tea EN-US strings
+sab4401="Experimental Shield Projector"
+sab4401_name="Pillar of Prominence"
+Unit_Description__BubbleTea_A4_01="Experimental shield projection facility. Generates protective bubbles around each individual structure within its operational radius."
+
+srb4401="Experimental Shield Generator"
+seb4401_name="BFS - Stalwart"
+Unit_Description__BubbleTea_A4_02="Generates a heavy shield around units and structures within its operational radius."
+
+ssb4401_desc="Experimental Shield Lattice"
+ssb4401_name="Iaathan"
+ssb4401_drone_desc="Experimental Shield Drone"
+ssb4401_large_name="Iaathanah"
+ssb4401_small_name="Iaathanahee"
+Unit_Description__BubbleTea_A4_03="Experimental shield facility. Generates a lattice of shield bubbles that increase in strength towards the centre. Lattice drones orbit the facility to distribute damage."
+
+# BrewResearch
+# Translater:HaoJun0823
+--ISO_639-1_code=EN
+--Author: Balthazar
+
+--Research centres
+seb9301_desc="Research Center"
+--UEF
+seb9101_name="Tyson Campus"
+seb9201_name="Sagan Campus"
+seb9301_name="Hawking Campus"
+--Cybran
+srb9101_name="Einstein Facility"
+srb9201_name="Schrodinger Facility"
+srb9301_name="Heisenberg Facility"
+--Aeon
+sab9101_name="Insight"
+sab9201_name="Guidance"
+sab9301_name="Enlightenment"
+--Seraphim
+ssb9101_name="Iyathlabistle"
+ssb9201_name="Iyathlab"
+ssb9301_name="Iyathluub"
+
+Unit_Description_RND_005="Basic research facility. Grants access to new units and higher tech levels."
+Unit_Description_RND_006="Intermediate research facility. Has increased research speed and durability."
+Unit_Description_RND_007="Advanced research facility. Has increased research speed and durability."
+
+--Science Centres
+seb9302_desc="Science Center"
+
+seb9202_name="TBD"
+seb9302_name="TBD"
+
+srb9202_name="TBD"
+srb9302_name="TBD"
+
+sab9202_name="TBD"
+sab9302_name="TBD"
+
+ssb9202_name="TBD"
+ssb9302_name="TBD"
+
+sb9302_desc="Provides adjacency bonuses to research centers, decreasing research time."
+sb9303_desc="Provides adjacency bonuses to research centers, decreasing research costs."
+
+--Research item information
+ability_rnd_unlock="Research Unlock"
+ability_rnd_command="Built by command unit"
+ability_rnd_engineer="Built by engineer"
+ability_rnd_field="Built by field engineer"
+ability_rnd_factory="Built by factory"
+ability_rnd_gantry="Built by experimental factory"
+ability_rnd_wall="Built on wall"
+ability_rnd_upgrade="Built as upgrade"
+
+--Research categories
+srnd9100_desc="Tech Level Research"
+srnd9200_desc="Tech Level Research"
+srnd9300_desc="Tech Level Research"
+srnd9400_desc="Experimental Tech Level Research"
+
+Unit_Description_RND_001="Unlocks additional tech 1 units and unit research."
+Unit_Description_RND_002="Unlocks tech 2 units and unit research."
+Unit_Description_RND_003="Unlocks tech 3 units and unit research."
+Unit_Description_RND_004="Unlocks experimental units and unit research."
+
+
+--Research unlock factories
+sb0105_desc="Hovercraft Factory"
+Unit_Description_RND_013="Produces amphibious units."
+--Specifically just surface amphib, and hover. Includes ekranoplans.
+sb0106_desc="Submarine Factory"
+Unit_Description_RND_014="Produces submersible units."
+--Submarines and maybe seabed amphibious
+sb0106_desc="Seaplane Factory"
+Unit_Description_RND_015="Produces seaplanes"
+--Seaplane factory might not get made.
+
+----------------------------------------
+-- Tech 1
+----------------------------------------
+
+--Wind turbines
+sb1101_desc="Wind Generator"
+seb1101_name="Turbine"
+sab1101_name="Lamp"
+ssb1101_name="Uyalai"
+srb1101_name="W.A.T"
+--Wind Assisted Turbine
+Unit_Description_RND_011="Generates a fluctuating quantity of energy."
+
+--Tidal generators
+sb1102_desc="Tidal Generator"
+seb1102_name="Tide Pod"
+sab1102_name="Flow"
+ssb1102_name="Uyaluos"
+srb1102_name="Red Lagoon"
+Unit_Description_RND_017="Generates an undulating quantity of energy. More effective with large bodies of water."
+
+----------------------------------------
+--Tech 2
+----------------------------------------
+
+--Power Generators (250 output)
+--Seraphim non-rotary power gens are all called Uya-iya
+--Cybran and Aeon tech 2's have no name
+seb1201_name="EG–100 Fusion Reactor"
+Unit_Description_RND_018="Low to mid-level power generator. Construct next to other structures for adjacency bonus."
+
+
+----------------------------------------
+--Tech 3
+----------------------------------------
+
+
+--Ekranoplan transports
+ss0326_desc="Heavy GEV Transport"
+ses0326_name="КМ66 Seamonster"
+srs0326_name="Deltawing"
+sas0326_name="Lun Mallard"
+sss0326_name="Vishwiseyen"
+
+# BrewReinforce
+
+# BrewMonsters
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Magnum Dong en strings
+
+srl0402_desc="Experimental Siege Tank"
+srl0402_name="Little Man"
+--Replaced with the Retributor
+
+srl0403_desc="Experimental Mobile Artillery"
+srl0403_name="Retributor"
+Unit_Description_RND_016="Large mobile heavy artillery. Must be stationary and on land to fire the artillery. Consumes large amounts of energy per shot"
+
+tooltipbrewlanRND0000="Toggle artillery"
+tooltipbrewlanRND0001="Enables or disables main artillery. Disabling prevents the artillery from potentially cancelling move orders."
+
+# BrewIntel
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Tea Party strings
+--Dedicated Omni Sensors
+sb3301_desc="Omni Sensor Array"
+seb3301_name="Welchman"
+srb3301_name="Qain"
+sab3301_name="Heimdallr"
+ssb3302_name="Selaez"
+Unit_Description_BrewIntel_001="Sensor array. Capable of identifying targets through stealth and cloak and clears jammer blips."
+
+sel4301_desc="Experimental Mobile Sensor"
+sel3401_name="Teatut"
+Unit_Description_BrewIntel_002="Experimental amphibious mobile sensor platform. Deploys with a directional sensor capable of identifying targets through stealth and cloak."
+ability_directional_sensor="Directional Sensor"
+
+# BrewAir
+# Translater:HaoJun0823
+--ISO_639-1_code=EN/CN
+--Penetrator strings
+----------------------------------------
+-- Tech 2
+----------------------------------------
+sa0212_desc="Combat Fighter"
+sea0212_name="Raptor"
+sra0212_name="Dioscuri"
+ssa0212_name="Iayath"
+Unit_Description_BrewPen_001="Mid-level air fighter. Excellent AA capabilities. Effective against enemy gunships and bombers."
+
+sa0213_desc="Fighter/Bomber"
+saa0213_name="Lucidity"
+Unit_Description_BrewPen_002="Combination fighter/bomber designed to engage both land and aerial units."
+
+--Dedicated Tactical Bombers
+saa0211_desc="Tactical Bomber"
+sea0211_name="Tornado"
+ssa0211_name="Sinn"
+sra0211_name="Condor"
+Unit_Description_BrewPen_003="Intermediate bomber aircraft."
+
+----------------------------------------
+-- Tech 3
+----------------------------------------
+
+sea0312_desc="Strike Fighter"
+sea0312_name="Fulcrum"
+Unit_Description_BrewPen_012="High-end multirole fighter. Equipped with missiles. Performs best against light armored targets such as aircraft."
+
+--T3.5 Advanced bombers
+--sa0314_desc="Penetrator"
+sa0314_desc="Penetrator Bomber"
+sea0314_name="Lancer"
+saa0314_name="Shrieker"
+sra0314_name="Sanguine Tyrant"
+ssa0314_name="Sinnaino"
+Unit_Description_BrewPen_004="Top-end long-range stealth strategic bomber. Equipped with bombs, air-to-surface standoff missiles, torpedoes, and rear-mounted anti-missile flares."
+
+--anti-SAM and anti-AAM flares
+ability_missiledef="Missile Defense"
+
+--T3.5 advanced fighters
+sa0313_desc="Penetration Fighter"
+sea0313_name="Tomcat"
+saa0313_name="Helios"
+sra0313_name="Twilight Patron"
+ssa0313_name="Ialosaare"
+Unit_Description_BrewPen_005="Top-end long range stealth fighter. Equipped with twin burst helical railguns, air-to-air missiles, and anti-missile flares."
+Unit_Description_BrewPen_006="Top-end long range stealth fighter. Equipped with twin focused phason laser weapons, and anti-missile flares."
+Unit_Description_BrewPen_007="Top-end long-range stealth fighter. Equipped with heavy particle cannons, and anti-missile flares. Capable of hunting spy planes."
+Unit_Description_BrewPen_008="Top-end long-range stealth fighter. Equipped with a Losaare autocannon, a phason weapon, and anti-missile flares."
+
+sra0315_desc="Interdiction Gunship"
+sra0315_name="Xiphos"
+Unit_Description_BrewPen_009="Heavy interdiction and close-air-support attack craft. Armed with multiple air-to-ground laser weapons, and anti-missile flares."
+
+sra0401_desc="Experimental Aircraft Carrier"
+sra0401_name="Simurgh"
+Unit_Description_BrewPen_010="Massive airborne aircraft carrier. Can create, transport, and deploy large numbers of fighter/bomber craft."
+
+--dra0202_desc="Fighter/Bomber"
+sra0402_name="Talon"
+Unit_Description_BrewPen_011="Combination fighter/bomber aircraft designed to engage both air and surface units."
+
+
+
